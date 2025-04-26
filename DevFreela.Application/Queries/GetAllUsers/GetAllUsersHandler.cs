06452edf9f548f2b270c8f1af852833a95907b5c
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-04-26 15:20:14 -0300

    Queries for User - CQRS

---
+++ DevFreela.Application/Queries/GetAllUsers/GetAllUsersHandler.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Queries.GetAllUsers
+{
+    public class GetAllUsersHandler : IRequestHandler<GetAllUsersQuery, ResultViewModel<List<UserViewModel>>>
+    {
+        private readonly DevFreelaDbContext _context;
+        public GetAllUsersHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ResultViewModel<List<UserViewModel>>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _context.Users
+               .Include(u => u.Skills)
+                   .ThenInclude(u => u.Skill)
+               .Where(u => u.FullName.Contains(request.Search) || u.Email.Contains(request.Search))
+               .Skip(request.Page * request.Size)
+               .Take(request.Size)
+               .ToListAsync();
+
+            var model = users.Select(UserViewModel.FromEntity).ToList();
+
+            return ResultViewModel<List<UserViewModel>>.Success(model);
+        }
+    }
+}
---
+++ DevFreela.Application/Queries/GetAllUsers/GetAllUsersQuery.cs
@@ -0,0 +1,18 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Queries.GetAllUsers
+{
+    public class GetAllUsersQuery : IRequest<ResultViewModel<List<UserViewModel>>>
+    {
+        public GetAllUsersQuery(string search, int page, int size)
+        {
+            Search = search;
+            Page = page;
+            Size = size;
+        }
+        public string Search { get; set; } = string.Empty;
+        public int Page { get; set; } = 0;
+        public int Size { get; set; } = 3;
+    }
+}
