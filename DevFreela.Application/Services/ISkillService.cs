86683b2be7312ce67c66716ba4f6c92a71b2a548
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-04-26 13:25:18 -0300

    Fix SkillService

--- DevFreela.Application/Services/ISkillService.cs
+++ DevFreela.Application/Services/ISkillService.cs
@@ -1,7 +1,5 @@
 ﻿using DevFreela.Application.Models;
 using DevFreela.Core.Entities;
-using DevFreela.Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,35 +12,5 @@ namespace DevFreela.Application.Services
     {
         ResultViewModel<List<Skill>> GetAll(string search = "", int page = 0, int size = 3);
         ResultViewModel<int> Insert(CreateSkillInputModel model);
-    }
-
-    public class SkillService : ISkillService
-    {
-        private readonly DevFreelaDbContext _context;
-        public SkillService(DevFreelaDbContext context)
-        {
-            _context = context;
-        }
-        public ResultViewModel<List<Skill>> GetAll(string search = "", int page = 0, int size = 3)
-        {
-            var skills = _context.Skills
-                .AsNoTracking()
-                .Where(s => s.Description.Contains(search))
-                .Skip(page * size)
-                .Take(size)
-                .ToList();
-
-            return ResultViewModel<List<Skill>>.Success(skills);
-        }
-
-        public ResultViewModel<int> Insert(CreateSkillInputModel model)
-        {
-            var skill = new Skill(model.Description);
-
-            _context.Skills.Add(skill);
-            _context.SaveChanges();
-
-            return ResultViewModel<int>.Success(skill.Id);
-        }
     }
 }
