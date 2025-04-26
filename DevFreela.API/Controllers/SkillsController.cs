d80a1cbf3cbe4ad51f14b0f6f848ec3fb6ed7ef9
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-04-26 15:47:40 -0300

    SkillsController with CQRS

--- DevFreela.API/Controllers/SkillsController.cs
+++ DevFreela.API/Controllers/SkillsController.cs
@@ -1,7 +1,6 @@
-﻿using DevFreela.Application.Models;
-using DevFreela.Application.Services;
-using DevFreela.Core.Entities;
-using DevFreela.Infrastructure.Persistence;
+﻿using DevFreela.Application.Commands.InsertSkills;
+using DevFreela.Application.Queries.GetAllSkills;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
@@ -10,25 +9,25 @@ namespace DevFreela.API.Controllers
     [ApiController]
     public class SkillsController : ControllerBase
     {
-        private readonly ISkillService _service;
-        public SkillsController(ISkillService service)
+        private readonly IMediator _mediator;
+        public SkillsController(IMediator mediator)
         {
-            _service = service;
+            _mediator = mediator;
         }
 
         // GET api/skills
         [HttpGet]
-        public IActionResult GetAll(string search = "", int page = 0, int size = 3)
+        public async Task<IActionResult> GetAll(string search = "", int page = 0, int size = 3)
         {
-            var model = _service.GetAll(search, page, size);
-            return Ok(model);
+            var result = await _mediator.Send(new GetAllSkillsQuery(search, page, size));
+            return Ok(result);
         }
 
         // POST api/skills
         [HttpPost]
-        public IActionResult Post(CreateSkillInputModel model)
+        public async Task<IActionResult> Post(InsertSkillsCommand command)
         {
-            var result = _service.Insert(model);
+            var result = await _mediator.Send(command);
             if (!result.IsSuccess)
                 return BadRequest(result.Message);
 
