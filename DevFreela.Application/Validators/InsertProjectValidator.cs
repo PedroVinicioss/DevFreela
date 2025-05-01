f399ab061c4af1d37cc17e79950f3a32ca81745e
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-05-01 11:25:47 -0300

    feat: Validation basic with FluentValidation

---
+++ DevFreela.Application/Validators/InsertProjectValidator.cs
@@ -0,0 +1,27 @@
+﻿using DevFreela.Application.Commands.InsertProject;
+using FluentValidation;
+
+namespace DevFreela.Application.Validators
+{
+    public class InsertProjectValidator : AbstractValidator<InsertProjectCommand>
+    {
+        public InsertProjectValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                    .WithMessage("Title is required")
+                .MaximumLength(50)
+                    .WithMessage("Title must be at most 50 characters long");
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                    .WithMessage("Description is required")
+                .MaximumLength(500)
+                    .WithMessage("Description must be at most 500 characters long");
+
+            RuleFor(x => x.TotalCost)
+                .GreaterThan(0)
+                    .WithMessage("Total cost must be greater than 0");
+        }
+    }
+}
---
+++ DevFreela.Application/Validators/InsertUserValidador.cs
@@ -0,0 +1,21 @@
+﻿using DevFreela.Application.Commands.InsertUser;
+using FluentValidation;
+
+namespace DevFreela.Application.Validators
+{
+    public class InsertUserValidador : AbstractValidator<InsertUserCommand>
+    {
+        public InsertUserValidador() 
+        {
+            RuleFor(e => e.Email)
+                .NotEmpty()
+                    .WithMessage("Email is required")
+                .EmailAddress()
+                    .WithMessage("Email is not valid");
+
+            RuleFor(b => b.BirthDate)
+                .Must(d => d < DateTime.Now.AddYears(-18))
+                    .WithMessage("User must be at least 18 years old");
+        }
+    }
+}
